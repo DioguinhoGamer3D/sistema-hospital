e6db50785e4edb900a40d541299ea00a4f3f9aea
Diogo <diogo.silva@dcx.ufpbr.br>
2026-05-30 20:13:42 -0300

    atualizei o gitignore

---
+++ src/main/java/SistemaHospital/ConexaoDB.java
@@ -0,0 +1,23 @@
+package SistemaHospital;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+
+    private static final Dotenv dotenv  = Dotenv.load();
+    private static final String URL     = dotenv.get("DB_URL");
+    private static final String USUARIO = dotenv.get("DB_USER");
+    private static final String SENHA   = dotenv.get("DB_PASS");
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar com o banco: " + e.getMessage());
+        }
+    }
+}
